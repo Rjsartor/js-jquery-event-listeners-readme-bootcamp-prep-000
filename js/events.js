c16f81bba850202c6c59c45e147ee714f9c5ba2c
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-24 17:27:24 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function () {
+    alert('Hey!')
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $("img").addClass("tasty")
+  });
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if ($( "input:first").key == 73 ) {
+      alert('you pressed G');
+    }
+  });
+}
+
+// document.addEventListener('keydown', (event) => {
+//   const keyName = event.key;
+//   alert('keydown event\n\n' + 'key: ' + keyName);
+// });
+
 
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
 });
