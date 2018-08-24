70508a333fdd02cff73b1197b0107380d9f7f934
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-24 17:46:48 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt() {
 
 function pressIt() {
   $('form').on('keydown', function(key) {
-    if (key.which == 73 ) {
+    if (key.which == 71 ) {
       alert('you pressed G');
     }
   });
